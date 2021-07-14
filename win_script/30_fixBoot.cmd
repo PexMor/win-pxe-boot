6cd160a7ec656d10b53cb2056a795c24019ceca6
PexMor <p.moravek@xlinux.cz>
2021-07-14 16:15:27 +0200

    add bcd removal

--- win_script/30_fixBoot.cmd
+++ win_script/30_fixBoot.cmd
@@ -1,9 +1,14 @@
 @echo off
+echo "Unhide and unsystem the BCD file"
+attrib -h -s w:\boot\bcd
+echo "Remote the obsolete BCD file"
+del w:\boot\bcd
 echo "Clean boot and master boot sector"
 bootsect w: /mbr
-echo "Clean w:\boot\bcd"
-bcdboot w: /bcdclean full
 echo "Add entry to w:\windows"
 bcdboot w:\windows /s w: /f bios
 echo "Shortend the timeout 30 -> 3 seconds"
-bcdedit /store w:\boot\bcd /timeout 10
+bcdedit /store w:\boot\bcd /timeout 3
+rem works only in full windows
+rem echo "Clean w:\boot\bcd"
+rem bcdboot /bcdclean full
