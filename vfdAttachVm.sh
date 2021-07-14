bb074bd9e10adf7fdb48f0b7c18a8d370b6634f7
PexMor <p.moravek@xlinux.cz>
2021-07-14 15:37:58 +0200

    fix VFD path

--- vfdAttachVm.sh
+++ vfdAttachVm.sh
@@ -3,5 +3,9 @@
 : ${DOM:=wimtest}
 : ${IDN:=rwdata/vfd/floppy.vfd}
 
+IDN=`readlink -f "$IDN"`
+
+echo "Dettach the VFD to fda if attached"
 virsh change-media $DOM fda --eject
+echo "Attach the VFD to fda"
 virsh change-media $DOM fda $IDN --insert
